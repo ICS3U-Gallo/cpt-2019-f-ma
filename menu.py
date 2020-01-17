c660a5c069ded9896208800277160040f73720ed
JzmR03 <54897930+JzmR03@users.noreply.github.com>
2020-01-17 09:24:27 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,16 +1,24 @@
 import arcade
-
 import settings
 
 
 class MenuView(arcade.View):
     def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.SKY_BLUE)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Menu", settings.WIDTH/2, settings.HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
+        arcade.draw_rectangle_filled(320, 180, 50, 500, arcade.color.BROWN)
+        arcade.draw_rectangle_filled(640, 165, 1280, 50, arcade.color.GREEN)
+        arcade.draw_rectangle_filled(320, 550, 300, 300, arcade.color.GREEN)
+        arcade.draw_rectangle_filled(250, 630, 50, 50, arcade.color.RED)
+        arcade.draw_rectangle_filled(380, 580, 50, 50, arcade.color.RED)
+        arcade.draw_text("Menu", settings.WIDTH/2, settings.HEIGHT/1.5,
+                         arcade.color.BLACK, font_size=35, anchor_x="center")
+        arcade.draw_text("Play", settings.WIDTH/2, settings.HEIGHT/2,       
+                         arcade.color.BLACK, font_size=35,  anchor_x="center")
+        arcade.draw_text("Press P", settings.WIDTH/2, settings.HEIGHT/3,       
+                         arcade.color.BLACK, font_size=35,  anchor_x="center")
 
     def on_key_press(self, key, modifiers):
         self.director.next_view()
@@ -19,10 +27,8 @@ class MenuView(arcade.View):
 if __name__ == "__main__":
     """This section of code will allow you to run your View
     independently from the main.py file and its Director.
-
     You can ignore this whole section. Keep it at the bottom
     of your code.
-
     It is advised you do not modify it unless you really know
     what you are doing.
     """
