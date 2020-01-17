c176f12d47493078d71d06fcd2b4501232d1a803
JzmR03 <54897930+JzmR03@users.noreply.github.com>
2020-01-17 09:26:54 -0500

    Update chapter_3.py

--- chapter_3.py
+++ chapter_3.py
@@ -21,13 +21,6 @@ class Chapter3View(arcade.View):
 
 
 if __name__ == "__main__":
-    """This section of code will allow you to run your View
-    independently from the main.py file and its Director.
-    You can ignore this whole section. Keep it at the bottom
-    of your code.
-    It is advised you do not modify it unless you really know
-    what you are doing.
-    """
     from utils import FakeDirector
     window = arcade.Window(settings.WIDTH, settings.HEIGHT)
     my_view = Chapter3View()
